f6c349499111234f6ab28334b84e5f5819b52c27
Илья <ily470@yandex.ru>
2023-03-12 14:18:21 +0700

    1. Двунаправленный связанный список

---
+++ src/test/LinkedListTest.java
@@ -0,0 +1,10 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+class LinkedListTest {
+    @Test
+    public void whenGeneratedLabels() {
+//        assertThat(strings).hasSameElementsAs(expected);
+    }
+    }
