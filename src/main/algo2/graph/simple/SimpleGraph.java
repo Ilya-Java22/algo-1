af15d198c602bc7655900119d12bafa01f0cef98
Илья <ily470@yandex.ru>
2023-09-08 01:05:03 +0700

    Поиск пути в графе (обход в глубину)

--- src/main/algo2/graph/simple/SimpleGraph.java
+++ src/main/algo2/graph/simple/SimpleGraph.java
@@ -1,11 +1,17 @@
 package main.algo2.graph.simple;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+
 class Vertex
 {
     public int Value;
+    public boolean Hit;
     public Vertex(int val)
     {
         Value = val;
+        Hit = false;
     }
 }
 
@@ -30,13 +36,13 @@ class SimpleGraph
                 break;
             }
         }
-        // ваш код добавления новой вершины
-        // с значением value
-        // в незанятую позицию vertex
+        // РІР°С€ РєРѕРґ РґРѕР±Р°РІР»РµРЅРёСЏ РЅРѕРІРѕР№ РІРµСЂС€РёРЅС‹
+        // СЃ Р·РЅР°С‡РµРЅРёРµРј value
+        // РІ РЅРµР·Р°РЅСЏС‚СѓСЋ РїРѕР·РёС†РёСЋ vertex
     }
 
-    // здесь и далее, параметры v -- индекс вершины
-    // в списке  vertex
+    // Р·РґРµСЃСЊ Рё РґР°Р»РµРµ, РїР°СЂР°РјРµС‚СЂС‹ v -- РёРЅРґРµРєСЃ РІРµСЂС€РёРЅС‹
+    // РІ СЃРїРёСЃРєРµ  vertex
     public void RemoveVertex(int v)
     {
         vertex[v] = null;
@@ -44,26 +50,65 @@ class SimpleGraph
             m_adjacency[i][v] = 0;
             m_adjacency[v][i] = 0;
         }
-        // ваш код удаления вершины со всеми её рёбрами
+        // РІР°С€ РєРѕРґ СѓРґР°Р»РµРЅРёСЏ РІРµСЂС€РёРЅС‹ СЃРѕ РІСЃРµРјРё РµС‘ СЂС‘Р±СЂР°РјРё
     }
 
     public boolean IsEdge(int v1, int v2)
     {
-        // true если есть ребро между вершинами v1 и v2
+        // true РµСЃР»Рё РµСЃС‚СЊ СЂРµР±СЂРѕ РјРµР¶РґСѓ РІРµСЂС€РёРЅР°РјРё v1 Рё v2
         return m_adjacency[v1][v2] == 1 || m_adjacency[v2][v1] == 1;
     }
 
     public void AddEdge(int v1, int v2)
     {
-        // добавление ребра между вершинами v1 и v2
+        // РґРѕР±Р°РІР»РµРЅРёРµ СЂРµР±СЂР° РјРµР¶РґСѓ РІРµСЂС€РёРЅР°РјРё v1 Рё v2
         m_adjacency[v1][v2] = 1;
         m_adjacency[v2][v1] = 1;
     }
 
     public void RemoveEdge(int v1, int v2)
     {
-        // удаление ребра между вершинами v1 и v2
+        // СѓРґР°Р»РµРЅРёРµ СЂРµР±СЂР° РјРµР¶РґСѓ РІРµСЂС€РёРЅР°РјРё v1 Рё v2
         m_adjacency[v1][v2] = 0;
         m_adjacency[v2][v1] = 0;
+    }
+
+    public ArrayList<Vertex> DepthFirstSearch(int VFrom, int VTo) {
+        Deque<Vertex> vertexStack = new ArrayDeque<>();
+        Vertex currentVertex = vertex[VFrom];
+        boolean foundAdjacentVertex;
+        while (currentVertex != null) {
+            foundAdjacentVertex = false;
+            if (!currentVertex.Hit) {
+                currentVertex.Hit = true;
+                vertexStack.push(currentVertex);
+            }
+            int currentVertexIndex = searchIndex(currentVertex);
+            for (int i = 0; i < max_vertex; i++) {
+                if (m_adjacency[currentVertexIndex][i] == 1 && vertex[i] == vertex[VTo]) {
+                    vertexStack.push(vertex[VTo]);
+                    return new ArrayList<>(vertexStack);
+                }
+                if (m_adjacency[currentVertexIndex][i] == 1 && i != currentVertexIndex && !vertex[i].Hit) {
+                    currentVertex = vertex[i];
+                    foundAdjacentVertex = true;
+                    break;
+                }
+            }
+            if (!foundAdjacentVertex) {
+                vertexStack.pop();
+                currentVertex = vertexStack.peek();
+            }
+        }
+        return new ArrayList<>();
+    }
+
+    private int searchIndex(Vertex searchingVertex) {
+        for (int i = 0; i < max_vertex; i++) {
+            if (vertex[i] == searchingVertex) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
