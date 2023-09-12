aecea780859cfb87ba7d8ca89bc04009a5a038a6
Илья <ily470@yandex.ru>
2023-09-13 00:46:14 +0700

    Уязвимые графы

--- src/main/algo2/graph/simple/SimpleGraph.java
+++ src/main/algo2/graph/simple/SimpleGraph.java
@@ -1,6 +1,7 @@
 package main.algo2.graph.simple;
 
 import java.util.*;
+import java.util.stream.IntStream;
 
 class Vertex
 {
@@ -80,33 +81,6 @@ class SimpleGraph
         m_adjacency[v2][v1] = 0;
     }
 
-    //не работает с циклами
-    public ArrayList<Vertex> DepthFirstSearchWOCycles(int VFrom, int VTo) {
-        Deque<Vertex> vertexStack = new ArrayDeque<>();
-        clearVerticesHitsAndTracks();
-        vertex[VFrom].Hit = true;
-        vertexStack.push(vertex[VFrom]);
-        while (!vertexStack.isEmpty()) {
-            Vertex currentVertex = vertexStack.peek();
-            int currentVertexIndex = searchIndex(currentVertex);
-            for (int i = 0; i < max_vertex; i++) {
-                if (m_adjacency[currentVertexIndex][i] == 1 && i == VTo) {
-                    return formResultList(vertexStack, VTo);
-                }
-                if (m_adjacency[currentVertexIndex][i] == 1 && !vertex[i].Hit) {
-                    vertex[i].Hit = true;
-                    vertexStack.push(vertex[i]);
-                    break;
-                }
-                if (i == max_vertex - 1) {
-                    vertexStack.pop();
-                }
-            }
-        }
-        return new ArrayList<>();
-    }
-
-    //работает и при поиске цикла, за это - вакханалия в for) но вообще в код в принципе нагроможден, кажется) сравни с более поздней версией выше без циклов
     public ArrayList<Vertex> DepthFirstSearch(int VFrom, int VTo) {
         Deque<Vertex> vertexStack = new ArrayDeque<>();
         clearVerticesHitsAndTracks();
@@ -172,7 +146,6 @@ class SimpleGraph
         }
     }
 
-    //не работает при поиске цикла, но зато лаконично
     public ArrayList<Vertex> BreadthFirstSearch(int VFrom, int VTo)
     {
         Queue<Vertex> vertexQueue = new LinkedList<>();
@@ -197,5 +170,43 @@ class SimpleGraph
             }
         }
         return new ArrayList<>();
+    }
+
+    public ArrayList<Vertex> WeakVertices()
+    {
+        Set<Vertex> resultSet = new HashSet<>();
+        Set<Vertex> strongVertices = new HashSet<>();
+        for (int i = 0; i < max_vertex; i++) {
+            if (strongVertices.contains(vertex[i])) {
+                continue;
+            }
+            int finalI = i;
+            List<Integer> adjacentVerticesIndexes = IntStream.range(0, max_vertex)
+                    .filter(j -> finalI != j && m_adjacency[finalI][j] == 1 && !resultSet.contains(vertex[j]))
+                    .boxed()
+                    .toList();
+            int[] foundInListAdjacentVertices = searchAdjacentVertices(adjacentVerticesIndexes);
+            if (foundInListAdjacentVertices.length == 0) {
+                resultSet.add(vertex[i]);
+                continue;
+            }
+            strongVertices.addAll(
+                    List.of(
+                            vertex[i],
+                            vertex[foundInListAdjacentVertices[0]],
+                            vertex[foundInListAdjacentVertices[1]]));
+        }
+        return new ArrayList<>(resultSet);
+    }
+
+    private int[] searchAdjacentVertices(List<Integer> adjacentVerticesIndexes) {
+        for (int i = 0; i < adjacentVerticesIndexes.size() - 1; i++) {
+            for (int j = i + 1; j < adjacentVerticesIndexes.size(); j++) {
+                if (IsEdge(adjacentVerticesIndexes.get(i), adjacentVerticesIndexes.get(j))) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
     }
 }
