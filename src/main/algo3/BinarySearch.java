a6f2bef6e5223bcbf5bdc4cb0b9272ccf5f4b89a
Илья <ily470@yandex.ru>
2023-10-01 01:10:09 +0700

    11. Двоичный поиск

--- src/main/algo3/BinarySearch.java
+++ src/main/algo3/BinarySearch.java
@@ -5,7 +5,6 @@ public class BinarySearch {
     public int Right;
     public int[] array;
     private int searchingStatus;
-    private boolean searchingIsOver;
 
     public BinarySearch(int[] array) {
         Left = 0;
@@ -14,15 +13,9 @@ public class BinarySearch {
     }
 
     public void Step(int N) {
-        if (searchingIsOver) {
-            Left = 0;
-            Right = array.length - 1;
-            searchingIsOver = false;
-        }
         int mid = (Left + Right) / 2;
         if (array[mid] == N) {
             searchingStatus = 1;
-            searchingIsOver = true;
             return;
         }
         if (N < array[mid]) {
@@ -32,7 +25,6 @@ public class BinarySearch {
         }
         if (Left >= Right) {
             searchingStatus = Left == Right && array[Left] == N ? 1 : -1;
-            searchingIsOver = Left == Right && array[Left] == N;
             return;
         }
         searchingStatus = 0;
@@ -40,5 +32,26 @@ public class BinarySearch {
 
     public int GetResult() {
         return searchingStatus;
+    }
+
+    public boolean GallopingSearch(int[] array, int N) {
+        searchingStatus = 0;
+        int i = 1;
+        int index;
+        while ((index = (int) (Math.pow(2,i) - 2)) < array.length) {
+            if (array[index] == N) {
+                return true;
+            }
+            if (array[index] > N) {
+                break;
+            }
+            i++;
+        }
+        Left = i == 1 ? 0 : (int) (Math.pow(2,i - 1) - 2) + 1;
+        Right = index < array.length ? index : array.length - 1;
+        while (searchingStatus == 0) {
+            Step(N);
+        }
+        return searchingStatus == 1;
     }
 }
