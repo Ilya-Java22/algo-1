1a9e3e490b8eb9ec7f28f888dbc8333078ea2c26
Илья <ily470@yandex.ru>
2023-09-30 18:48:35 +0700

    11. Двоичный поиск

---
+++ src/main/algo3/BinarySearch.java
@@ -0,0 +1,33 @@
+package main.algo3;
+
+public class BinarySearch {
+    public int Left;
+    public int Right;
+    public int[] array;
+    private int searchingStatus;
+
+
+    public BinarySearch(int[] array) {
+        Left = 0;
+        Right = array.length - 1;
+        this.array = array;
+    }
+
+    public void Step(int N) {
+        int mid = (Left + Right) / 2;
+        if (array[mid] == N) {
+            searchingStatus = 1;
+            return;
+        }
+        if (N < array[mid]) {
+            Right = mid - 1;
+        } else {
+            Left = mid + 1;
+        }
+        searchingStatus = Right - Left < 2 ? -1 : 0;
+    }
+
+    public int GetResult() {
+        return searchingStatus;
+    }
+}
